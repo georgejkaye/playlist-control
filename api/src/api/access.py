6080732635b3e608077b6c7b53824229a2de10df
George Kaye <georgejkaye@gmail.com>
2024-03-04 23:19:31 +0000

    [impl] Change to using spotipy to auth and access

--- api/src/api/access.py
+++ api/src/api/access.py
@@ -1,24 +1,34 @@
 from api.utils import get_env_variable, get_secret
-import requests
 
-def get_access_token() -> str:
-    token_url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    client_id = get_env_variable("CLIENT_ID")
-    client_secret = get_secret("CLIENT_SECRET")
-    data = {
-        "grant_type": "client_credentials",
-        "client_id": client_id,
-        "client_secret": client_secret
-    }
-    response = requests.post(
-        token_url, headers=headers, data=data
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy import Spotify
+
+scope = [
+    "user-library-read",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+]
+
+
+def authorise_access() -> Spotify:
+    client_id = get_env_variable("SPOTIFY_ID")
+    client_secret = get_secret("SPOTIFY_SECRET")
+    redirect_uri = get_env_variable("SPOTIFY_REDIRECT")
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            scope=scope,
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+        )
     )
-    if response.status_code == 200:
-        json = response.json()
-        token = json["access_token"]
-        return token
-    else:
-        raise RuntimeError(f"Could not get access token: Error {response.status_code}")
+    user = sp.current_user()
+    if user:
+        print(f"Thank you for authorising, {user['display_name']}!")
+    return sp
+
+
+if __name__ == "__main__":
+    authorise_access()
