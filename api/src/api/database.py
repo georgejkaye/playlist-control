28324dbdfa6e6f5bcc8485c472bdec7fa91828e2
George Kaye <georgejkaye@gmail.com>
2024-03-06 00:02:44 +0000

    [feat] Add insert breaks database method

--- api/src/api/database.py
+++ api/src/api/database.py
@@ -84,3 +84,85 @@ def select_tracks() -> list[Track]:
         )
         tracks.append(track)
     return tracks
+
+
+def get_args_statement(cur, tups: set[tuple]) -> str:
+    columns = len(list(tups)[0])
+    percents = ["%s" for i in range(0, columns)]
+    placeholders = f"({','.join(percents)})"
+    args_str = ",".join(cur.mogrify(placeholders, x).decode("utf-8") for x in tups)
+    return args_str
+
+
+def get_insert_artist_statement(cur, artists: set[tuple[str, str]]) -> str:
+    return f"""
+            INSERT INTO Artist (artist_id, artist_name) VALUES {get_args_statement(cur, artists)}
+            ON CONFLICT (artist_id) DO NOTHING
+    """
+
+
+def get_insert_album_statement(cur, albums: set[tuple[str, str, str]]) -> str:
+    return f"""
+            INSERT INTO Album (album_id, album_name, album_art) VALUES {get_args_statement(cur, albums)}
+            ON CONFLICT (album_id) DO NOTHING
+    """
+
+
+def get_insert_track_statement(cur, tracks: set[tuple[str, str, str]]) -> str:
+    return f"""
+            INSERT INTO Track (track_id, track_name, track_duration) VALUES {get_args_statement(cur, tracks)}
+            ON CONFLICT (track_id) DO NOTHING
+    """
+
+
+def get_insert_album_track_statement(cur, album_tracks: set[tuple[str, str]]):
+    return f"""
+            INSERT INTO AlbumTrack (album_id, track_id) VALUES {get_args_statement(cur, album_tracks)}
+    """
+
+
+def get_insert_artist_track_statement(cur, artist_tracks: set[tuple[str, str]]):
+    return f"""
+        INSERT INTO ArtistTrack (artist_id, track_id) VALUES {get_args_statement(cur, artist_tracks)}
+    """
+
+
+def get_insert_album_artist_statement(cur, album_artists: set[tuple[str, str]]):
+    return f"""
+        INSERT INTO AlbumArtist (album_id, artist_id) VALUES {get_args_statement(cur, album_artists)}
+    """
+
+
+def execute_many(cur, fn: Callable, tups: set[tuple]):
+    statement = fn(cur, tups)
+    cur.execute(statement)
+
+
+def insert_tracks(tracks: list[Track]):
+    (conn, cur) = connect()
+    artists = set()
+    albums = set()
+    album_tracks = set()
+    artist_tracks = set()
+    album_tracks = set()
+    album_artists = set()
+    track_tuples = set()
+    for track in tracks:
+        album = track.album
+        albums.add((album.id, album.name, album.art))
+        for artist in album.artists:
+            artists.add((artist.id, artist.name))
+            album_artists.add((album.id, artist.id))
+        album_tracks.add((album.id, track.id))
+        for artist in track.artists:
+            artists.add((artist.id, artist.name))
+            artist_tracks.add((artist.id, track.id))
+        track_tuples.add((track.id, track.name, track.duration))
+    execute_many(cur, get_insert_artist_statement, artists)
+    execute_many(cur, get_insert_album_statement, albums)
+    execute_many(cur, get_insert_track_statement, track_tuples)
+    execute_many(cur, get_insert_album_artist_statement, album_artists)
+    execute_many(cur, get_insert_artist_track_statement, artist_tracks)
+    execute_many(cur, get_insert_album_track_statement, album_tracks)
+    conn.commit()
+    conn.close()
