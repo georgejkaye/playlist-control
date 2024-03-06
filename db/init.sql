a4ef63453fdeb890b7e1e2eee99168b03902c08b
George Kaye <georgejkaye@gmail.com>
2024-03-06 00:26:08 +0000

    [feat] Add db init script

---
+++ db/init.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Artist (
+    artist_id TEXT PRIMARY KEY,
+    artist_name TEXT NOT NULL
+);
+
+CREATE TABLE Album (
+    album_id TEXT PRIMARY KEY,
+    album_name TEXT NOT NULL,
+    album_art TEXT NOT NULL
+);
+
+CREATE TABLE AlbumArtist (
+    album_id TEXT NOT NULL,
+    artist_id TEXT NOT NULL,
+    CONSTRAINT fk_album_id FOREIGN KEY(album_id) REFERENCES album(album_id) ON DELETE CASCADE,
+    CONSTRAINT fk_artist_id FOREIGN KEY(artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE,
+    UNIQUE (album_id, artist_id)
+);
+
+CREATE TABLE Track (
+    track_id TEXT PRIMARY KEY,
+    track_name TEXT NOT NULL,
+    track_duration INT NOT NULL
+);
+
+CREATE TABLE ArtistTrack (
+    artist_id TEXT NOT NULL,
+    track_id TEXT NOT NULL,
+    CONSTRAINT fk_artist_id FOREIGN KEY(artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE,
+    CONSTRAINT fk_track_id FOREIGN KEY(track_id) REFERENCES track(track_id) ON DELETE CASCADE,
+    UNIQUE (artist_id, track_id)
+);
+
+CREATE TABLE AlbumTrack (
+    album_id TEXT NOT NULL,
+    track_id TEXT NOT NULL,
+    CONSTRAINT fk_album_id FOREIGN KEY(album_id) REFERENCES album(album_id) ON DELETE CASCADE,
+    CONSTRAINT fk_track_id FOREIGN KEY(track_id) REFERENCES track(track_id) ON DELETE CASCADE,
+    UNIQUE (album_id, track_id)
+)
