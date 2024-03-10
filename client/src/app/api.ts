2f40085362724b05940762f1ffbad85bcc669137
George Kaye <georgejkaye@gmail.com>
2024-03-10 23:39:57 +0000

    [feat] Add queue selection feature

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -26,6 +26,21 @@ const responseToCurrentTrack = (response: any) => ({
     start: response["start"]
 })
 
+export const getData = async (setTracks: SetState<Track[]>, setCurrent: SetState<CurrentTrack | undefined>, setQueue: SetState<Track[]>) => {
+    const endpoint = "/api/data"
+    const response = await axios.get(endpoint)
+    if (response.status === 200) {
+        const data = response.data
+        console.log(data)
+        const tracks = data["tracks"].map(responseToTrack)
+        const current = responseToCurrentTrack(data["current"])
+        const queue = data["queue"].map(responseToTrack)
+        setTracks(tracks)
+        setCurrent(current)
+        setQueue(queue)
+    }
+}
+
 export const getQueue = async (setCurrent: SetState<CurrentTrack | undefined>, setQueue: SetState<Track[]>) => {
     const endpoint = "/api/queue"
     const response = await axios.get(endpoint)
@@ -34,6 +49,19 @@ export const getQueue = async (setCurrent: SetState<CurrentTrack | undefined>, s
         const current = responseToCurrentTrack(data["current"])
         const queue = data["queue"].map(responseToTrack)
         setCurrent(current)
+        setQueue(queue)
+    }
+}
+
+export const postQueue = async (track: Track, setQueue: SetState<Track[]>) => {
+    const endpoint = "/api/queue"
+    const params = {
+        "track_id": track.id
+    }
+    const response = await axios.post(endpoint, null, { params })
+    if (response.status === 200) {
+        const data = response.data
+        const queue = data.map(responseToTrack)
         setQueue(queue)
     }
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { CurrentTrack, Track, getMultipleArtistsString } from "./structs";
-import { getQueue } from "./api";
+import { CurrentTrack, SetState, Track, getMultipleArtistsString } from "./structs";
+import { getData, getQueue, postQueue } from "./api";
 
 const CurrentTrack = (props: { currentTrack: CurrentTrack }) => {
   return (
-    <div className="flex flex-row justify-center my-10 gap-10">
+    <div className="flex flex-row justify-center my-6 gap-10 mx-1">
       <div>
         <img className="rounded-lg" width="200" src={props.currentTrack.track.album.art} alt={`Album art for ${props.currentTrack.track.album.name}`} />
       </div>
@@ -26,8 +26,10 @@ const CurrentTrack = (props: { currentTrack: CurrentTrack }) => {
 
 }
 
+const trackCardStyle = "rounded-lg flex flex-row justify-center my-1 p-1 gap-5"
+
 const TrackCard = (props: { track: Track }) => {
-  return (<div key={props.track.id} className="flex flex-row justify-center my-2 gap-5 animate-fadein">
+  return (<div key={props.track.id} className={trackCardStyle} >
     <div>
       <img className="rounded-lg" width="50" src={props.track.album.art} alt={`Album art for ${props.track.album.name}`} />
     </div>
@@ -35,7 +37,7 @@ const TrackCard = (props: { track: Track }) => {
       <div className="text-xl font-bold">{props.track.name}</div>
       <div>{getMultipleArtistsString(props.track.artists)}</div >
     </div>
-  </div>
+  </div >
   )
 }
 
@@ -45,18 +47,82 @@ const Queue = (props: { queue: Track[] }) => {
   </div>)
 }
 
+const QueueAdderTrackCard = (props: { track: Track, setQueue: SetState<Track[]>, setAdding: SetState<boolean>, setLocked: SetState<boolean> }) => {
+  const onClickCard = (e: React.MouseEvent<HTMLDivElement>) => {
+    props.setLocked(true)
+    postQueue(props.track, props.setQueue)
+    props.setAdding(false)
+    props.setLocked(false)
+  }
+  return (<div key={props.track.id} className={`${trackCardStyle} hover:bg-gray-700 cursor-pointer`} onClick={onClickCard}>
+    <div>
+      <img className="rounded-lg" width="52" src={props.track.album.art} alt={`Album art for ${props.track.album.name}`} />
+    </div>
+    <div className="flex-1 my-auto">
+      <div className="text-xl font-bold">{props.track.name}</div>
+      <div>{getMultipleArtistsString(props.track.artists)}</div >
+    </div>
+  </div>
+  )
+}
+
+const defaultTracksToShow = 50
+const bigButtonStyle = "rounded-lg bg-gray-700 p-4 my-4 font-bold text-2xl cursor-pointer hover:bg-gray-600"
+
+const QueueAdder = (props: { isAdding: boolean, setAdding: SetState<boolean>, setLocked: SetState<boolean>, tracks: Track[], setCurrent: SetState<CurrentTrack | undefined>, setQueue: SetState<Track[]> }) => {
+  const [filteredTracks, setFilteredTracks] = useState<Track[]>(props.tracks.sort((t1, t2) => t1.name.localeCompare(t2.name)))
+  const [filterText, setFilterText] = useState("")
+  const [tracksToShow, setTracksToShow] = useState(100)
+  useEffect(() => {
+    setFilteredTracks(props.tracks.filter((track) => (
+      track.name.toLowerCase().includes(filterText.toLowerCase()) || getMultipleArtistsString(track.artists).toLowerCase().includes(filterText.toLowerCase()) || track.album.name.toLowerCase().includes(filterText.toLowerCase())
+    )).sort((t1, t2) => t1.name.localeCompare(t2.name)))
+  }, [filterText])
+  useEffect(() => {
+    setFilterText("")
+    setTracksToShow(defaultTracksToShow)
+  }, [props.isAdding])
+  const onChangeFilterText = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFilterText(e.target.value)
+  const onClickAdd = (e: React.MouseEvent<HTMLDivElement>) =>
+    props.setAdding(!props.isAdding)
+  const onClickMore = (e: React.MouseEvent<HTMLDivElement>) =>
+    setTracksToShow(tracksToShow + defaultTracksToShow)
+  return (<div className="m-1">
+    <div className={bigButtonStyle} onClick={onClickAdd}>{!props.isAdding ? "Add to queue" : "Back"}</div>
+    {!props.isAdding ? "" : <div>
+      <div className="flex">
+        <input className="rounded-lg text-black mb-4 flex-1 p-4 text-lg" type="text" value={filterText} placeholder={"Search"} onChange={onChangeFilterText} />
+      </div>
+      <div>
+        {filteredTracks.slice(0, tracksToShow).map((track) => <QueueAdderTrackCard track={track} setQueue={props.setQueue} setAdding={props.setAdding} setLocked={props.setLocked} />)}
+      </div>{tracksToShow >= filteredTracks.length ? "" : <div className={bigButtonStyle} onClick={onClickMore}>More</div>}</div>}
+  </div >)
+}
+
 const Home = () => {
   const [current, setCurrent] = useState<CurrentTrack | undefined>(undefined)
   const [queue, setQueue] = useState<Track[]>([])
+  const [tracks, setTracks] = useState<Track[]>([])
+  const [isAdding, setAdding] = useState(false)
+  const [isLocked, setLocked] = useState(false)
   useEffect(() => {
-    getQueue(setCurrent, setQueue)
+    getData(setTracks, setCurrent, setQueue)
+    console.log(tracks)
   }, [])
   useEffect(() => {
     const interval = setInterval(() => {
-      getQueue(setCurrent, setQueue)
+      if (!isLocked) {
+        getQueue(setCurrent, setQueue)
+      }
     }, 5000)
     return () => clearInterval(interval)
   })
+  useEffect(() => {
+    if (!isAdding) {
+      window.scrollTo({ top: 0 })
+    }
+  }, [isAdding])
   useEffect(() => {
     if (current) {
       console.log("Updating song")
@@ -73,9 +139,10 @@ const Home = () => {
   return (
     <main>
       {!current ? "" :
-        <div className="mx-8 my-6 desktop:mx-auto desktop:w-desktop">
+        <div className="mx-4 my-6 desktop:mx-auto desktop:w-desktop">
           <CurrentTrack currentTrack={current} />
-          <Queue queue={queue} />
+          <QueueAdder isAdding={isAdding} setAdding={setAdding} setLocked={setLocked} tracks={tracks} setCurrent={setCurrent} setQueue={setQueue} />
+          {isAdding ? "" : <Queue queue={queue} />}
         </div>
       }
     </main>
