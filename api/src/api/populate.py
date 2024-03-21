dc5328b5b80c504ffe8eeb7a87e110c0ec9bbf18
George Kaye <georgejkaye@gmail.com>
2024-03-21 20:20:34 +0000

    [tidy] Remove populate file

--- api/src/api/populate.py
+++
@@ -1,8 +0,0 @@
-import sys
-from api.database import insert_tracks
-from api.spotify import authorise_access, get_tracks_from_playlist
-
-sp = authorise_access()
-tracks = get_tracks_from_playlist(sp, sys.argv[1])
-if tracks is not None:
-    insert_tracks(tracks)
