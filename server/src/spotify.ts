0412407b337cae591419fcad21abf2f3b6a6d8a0
George Kaye <georgejkaye@gmail.com>
2024-03-28 22:08:38 +0000

    [impl] Add functions for parsing spotify api data

--- server/src/spotify.ts
+++ server/src/spotify.ts
@@ -87,3 +87,56 @@ export const getSpotifyUser = async (accessToken: string) => {
     return undefined
   }
 }
+
+const nameRegex =
+  /(.+)(( - |())(Radio Mix|Full Length Version|Radio Edit|Deluxe Edition)?((Remastered )?([0-9][0-9][0-9][0-9])?( Remastered( Version)?| Remaster| Mix)?)?()?)/
+
+const sanitiseName = (name: string) => {
+  let match = name.match(nameRegex)
+  if (!match) {
+    return name
+  } else {
+    return match[1]
+  }
+}
+
+const responseToArtist = (raw: any) => {
+  let id = raw.id
+  let name = raw.name
+  return { id, name }
+}
+
+const responseToAlbum = (raw: any) => {
+  let name = sanitiseName(raw.name)
+  let art = raw.images.length > 0 ? raw.images[0].url : ""
+  let artists = raw.artists.map(responseToArtist)
+  let id = raw.id
+  return { id, name, artists, art }
+}
+
+const responseToTrack = (raw: any) => {
+  let id = raw.id
+  let name = sanitiseName(raw.name)
+  let album = responseToAlbum(raw.album)
+  let artists = raw.artists.map(responseToArtist)
+  let duration = raw.duration_ms
+  return { id, name, album, artists, duration }
+}
+
+export const getCurrentTrack = async (accessToken: string) => {
+  const url = getApiURLFromEndpoint("/me/player/currently-playing")
+  const headers = getAuthHeader(accessToken)
+  try {
+    let response = await axios.get(url, { headers })
+    let data = response.data
+    let item = data.item
+    if (item) {
+      let track = responseToTrack(item)
+      return track
+    } else {
+      return undefined
+    }
+  } catch (e) {
+    return undefined
+  }
+}
