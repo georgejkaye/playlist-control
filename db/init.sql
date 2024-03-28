53312058dac4c39e18caf0ef3ab1e78341f52d0a
George Kaye <georgejkaye@gmail.com>
2024-03-28 20:54:30 +0000

    [impl] Modify database schema

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,14 @@
-CREATE TABLE Session (
-    session_id SERIAL PRIMARY KEY,
-    session_name TEXT NOT NULL,
-    playlist_id TEXT NOT NULL,
-    session_start TIMESTAMP WITHOUT TIME ZONE NOT NULL
+
+CREATE TABLE LocalUser (
+    user_id SERIAL PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    user_password_hash TEXT NOT NULL,
+    spotify_id TEXT,
+    access_token TEXT,
+    refresh_token TEXT,
+    expires_at TIMESTAMP WITH TIME ZONE,
+    session_name TEXT,
+    playlist_id TEXT
 );
 
 CREATE TABLE Artist (
@@ -29,8 +35,8 @@ CREATE TABLE Track (
     track_name TEXT NOT NULL,
     track_duration INT NOT NULL,
     queued_at TIMESTAMP WITHOUT TIME ZONE,
-    session_id INT NOT NULL,
-    CONSTRAINT fk_session_id FOREIGN KEY(session_id) REFERENCES session(session_id) ON DELETE CASCADE
+    user_id INT NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES LocalUser(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE ArtistTrack (
@@ -47,4 +53,6 @@ CREATE TABLE AlbumTrack (
     CONSTRAINT fk_album_id FOREIGN KEY(album_id) REFERENCES album(album_id) ON DELETE CASCADE,
     CONSTRAINT fk_track_id FOREIGN KEY(track_id) REFERENCES track(track_id) ON DELETE CASCADE,
     UNIQUE (album_id, track_id)
-)
+);
+
+INSERT INTO LocalUser (user_name, user_password_hash) VALUES ('admin', '$2a$10$Y/tdZWiBvQlb2u9bN5bYruxSvUZJ2XQtoJYGDldfj5t7FwXI.OAkO');
