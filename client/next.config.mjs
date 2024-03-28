5f117bf5a166adace2ac816878dd5f6dd5ce4149
George Kaye <georgejkaye@gmail.com>
2024-03-28 20:59:25 +0000

    [impl] Update client environment stuff

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,40 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${process.env.API_URL}/:path*`
-            },
-            {
-                source: "/server/:path*",
-                destination: `${process.env.SERVER_URL}/:path*`
-            },
-        ]
-    },
-    output: "standalone",
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "image-cdn-**.spotifycdn.com",
-            pathname: "/image/*"
-        }, {
-            protocol: "https",
-            hostname: "mosaic.scdn.co"
-        }, {
-            protocol: "https",
-            hostname: "i.scdn.co",
-            pathname: "/image/*"
-        }, {
-            hostname: "blend-playlist-covers.spotifycdn.com"
-        }, {
-            hostname: "lineup-images.scdn.co"
-        }, {
-            hostname: "wrapped-images.spotifycdn.com"
-        }, {
-            hostname: "newjams-images.scdn.co"
-        }]
-    }
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+      {
+        source: "/server/:path*",
+        destination: `${process.env.SERVER_URL}/:path*`,
+      },
+    ]
+  },
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image-cdn-**.spotifycdn.com",
+        pathname: "/image/*",
+      },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        pathname: "/image/*",
+      },
+      {
+        hostname: "blend-playlist-covers.spotifycdn.com",
+      },
+      {
+        hostname: "lineup-images.scdn.co",
+      },
+      {
+        hostname: "wrapped-images.spotifycdn.com",
+      },
+      {
+        hostname: "newjams-images.scdn.co",
+      },
+    ],
+  },
 }
 
 export default nextConfig
