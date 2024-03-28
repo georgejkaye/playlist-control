8842fb2146ed30b50564c3a5c7b1b2fd63d7ae30
George Kaye <georgejkaye@gmail.com>
2024-03-28 20:55:26 +0000

    [impl] Add some database methods

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,7 +1,7 @@
-import { readFileSync } from "fs"
 import { Client, connect } from "ts-postgres"
 import { Track } from "./structs.js"
 import { getSecret } from "./utils.js"
+import { SpotifyTokens, getSpotifyUser, refreshTokens } from "./spotify.js"
 
 const DB_HOST = process.env.DB_HOST || "georgejkaye.com"
 const DB_USER = process.env.DB_USER || "playlist"
@@ -23,6 +23,63 @@ const init = async () =>
   }))
 
 init()
+
+export const checkUserExists = async (username: string) => {
+  const queryText = "SELECT user_name FROM LocalUser WHERE user_name = $1"
+  const query = { text: queryText }
+  const result = await client.query(query, [username])
+  if (result.rows.length !== 1) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export const getPasswordHash = async (username: string) => {
+  const queryText =
+    "SELECT user_password_hash FROM LocalUser WHERE user_name = $1"
+  const query = { text: queryText }
+  const result = await client.query(query, [username])
+  if (result.rows.length !== 1) {
+    return undefined
+  } else {
+    let hashedPassword: string = result.rows[0].get("user_password_hash")
+    return hashedPassword
+  }
+}
+
+export const getAuthData = async (username: string) => {
+  const queryText =
+    "SELECT spotify_id, access_token, refresh_token, expires_at FROM LocalUser WHERE user_name = $1"
+  const query = { text: queryText }
+  let result = await client.query(query, [username])
+  let rows = result.rows
+  if (rows.length !== 1) {
+    return undefined
+  } else {
+    let row = rows[0]
+    let spotifyId = row.get("spotify_id")
+    if (!spotifyId) {
+      return undefined
+    } else {
+      var accessToken: string = row.get("access_token")
+      let refreshToken: string = row.get("refresh_token")
+      let expiresAt: Date = row.get("expires_at")
+      console.log(expiresAt)
+      if (expiresAt < new Date()) {
+        let tokens = await refreshTokens(refreshToken)
+        if (!tokens) {
+          return undefined
+        } else {
+          await updateTokens(username, tokens)
+          accessToken = tokens.access
+        }
+      }
+      let user = await getSpotifyUser(accessToken)
+      return user
+    }
+  }
+}
 
 const selectTracksAndArtists = `
     SELECT
@@ -62,4 +119,37 @@ export const getTracks = async (trackIds: String[]) => {
   const query = { text: queryText }
   const result = await client.query<Track>(query, [trackIds])
   return result
+}
+
+export const updateTokens = async (username: string, tokens: SpotifyTokens) => {
+  let user = await getSpotifyUser(tokens.access)
+  console.log(user)
+  if (user) {
+    const queryText = `
+      UPDATE LocalUser
+      SET spotify_id = $1, access_token = $2, refresh_token = $3, expires_at = $4
+      WHERE user_name = $5
+    `
+    const query = { text: queryText }
+    await client.query(query, [
+      user.id,
+      tokens.access,
+      tokens.refresh,
+      tokens.expires,
+      username,
+    ])
+  }
+}
+
+export const discardTokens = async (username: string) => {
+  const queryText = `
+    UPDATE LocalUser
+    SET
+      spotify_id = NULL, access_token = NULL,
+      refresh_token = NULL, expires_at = NULL
+    WHERE
+      username = $1
+  `
+  const query = { text: queryText }
+  await client.query(query, [username])
 }
